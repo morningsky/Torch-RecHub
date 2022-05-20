001510e8874ced851ff57a33e076597a8eb556ad
“morningsky” <sky_lmc@foxmail.com>
2022-05-20 08:29:10 +0800

    update dssm
    edit for Embedding-based Retrieval

--- torch_rechub/models/matching/dssm.py
+++ torch_rechub/models/matching/dssm.py
@@ -1,8 +1,9 @@
 """
-Date: create on 12/05/2022
+Date: create on 12/05/2022, update on 20/05/2022
 References: 
     paper: (CIKM'2013) Learning Deep Structured Semantic Models for Web Search using Clickthrough Data
     url: https://posenhuang.github.io/papers/cikm2013_DSSM_fullversion.pdf
+    code: https://github.com/bbruceyuan/DeepMatch-Torch/blob/main/deepmatch_torch/models/dssm.py
 Authors: Mincai Lai, laimincai@shanghaitech.edu.cn
 """
 
@@ -17,26 +18,52 @@ class DSSM(torch.nn.Module):
     Args:
         user_features (list[Feature Class]): training by the user tower module.
         item_features (list[Feature Class]): training by the item tower module.
-        mlp_params (dict): the params of the last MLP module, keys include:`{"dims":list, "activation":str, "dropout":float, "output_layer":bool`}
+        sim_func (str): similarity function, includes `["cosine", "dot"]`, default to "cosine".
+        temperature (float): temperature factor for similarity score, default to 1.0.
+        user_params (dict): the params of the User Tower module, keys include:`{"dims":list, "activation":str, "dropout":float, "output_layer":bool`}.
+        item_params (dict): the params of the Item Tower module, keys include:`{"dims":list, "activation":str, "dropout":float, "output_layer":bool`}.
     """
 
-    def __init__(self, user_features, item_features, user_params, item_params):
-        super(DSSM, self).__init__()
+    def __init__(self, user_features, item_features, sim_func="cosine", temperature=1.0, user_params={"dims":[32,16],"output_layer":False}, item_params={"dims":[32,16],"output_layer":False}):
+        super().__init__()
         self.user_features = user_features
         self.item_features = item_features
+        self.sim_func = sim_func
+        self.temperature = temperature
         self.user_dims = sum([fea.embed_dim for fea in user_features])
         self.item_dims = sum([fea.embed_dim for fea in item_features])
 
         self.embedding = EmbeddingLayer(user_features + item_features)
-        self.user_tower = MLP(self.user_dims, **user_params)
-        self.item_tower = MLP(self.item_dims, **item_params)
+        self.user_mlp = MLP(self.user_dims, **user_params)
+        self.item_mlp = MLP(self.item_dims, **item_params)
+        self.mode = None
 
     def forward(self, x):
-        input_user = self.embedding(x, self.user_features, squeeze_dim=True)  #[batch_size, num_features*deep_dims]
-        input_item = self.embedding(x, self.fm_features, squeeze_dim=True)  #[batch_size, num_features*embed_dim]
-
-        user_out = self.user_tower(input_user)
-        item_out = self.item_tower(input_item)
-        print(user_out.shape)
-        y = torch.cosine_similarity(user_out, item_out, dim=1)
+        user_embedding = self.user_tower(x)
+        item_embedding = self.item_tower(x)
+        if self.mode == "user_tower":
+            return user_embedding
+        if self.mode == "item_tower":
+            return item_embedding
+        if self.sim_func=="cosine":
+            y = torch.cosine_similarity(user_embedding, item_embedding, dim=1)
+            y = y/self.temperature
+        elif self.sim_func=="dot":
+            y = torch.mul(user_embedding, item_embedding).sum(dim=1)
+        else:
+            raise ValueError("similarity function only support %s, but got %s"%(["cosine", "dot"], self.sim_func))
         return torch.sigmoid(y)
+    
+    def user_tower(self, x):
+        if self.mode == "item_tower":
+            return None
+        input_user = self.embedding(x, self.user_features, squeeze_dim=True)  #[batch_size, num_features*deep_dims]
+        user_embedding = self.user_mlp(input_user) #[batch_size, user_params["dims"][-1]]
+        return user_embedding
+    
+    def item_tower(self, x):
+        if self.mode == "user_tower":
+            return None
+        input_item = self.embedding(x, self.item_features, squeeze_dim=True)  #[batch_size, num_features*embed_dim]
+        item_embedding = self.item_mlp(input_item) #[batch_size, item_params["dims"][-1]]
+        return item_embedding
